dd2aa880d38fc416e85fe49d029534cb5bc941d2
Moreno-S-Mariana <90338791+Moreno-S-Mariana@users.noreply.github.com>
2025-03-18 23:52:52 -0600

    PRACTICA 05
    RELEASE EN ZIP POR QUE PESA MUCHO

---
+++ P05-319170252.cpp
@@ -0,0 +1,404 @@
+/*
+Práctica 5: Optimización y Carga de Modelos
+*/
+//para cargar imagen
+#define STB_IMAGE_IMPLEMENTATION
+
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#include <vector>
+#include <math.h>
+
+#include <glew.h>
+#include <glfw3.h>
+
+#include <glm.hpp>
+#include <gtc\matrix_transform.hpp>
+#include <gtc\type_ptr.hpp>
+//para probar el importer
+//#include<assimp/Importer.hpp>
+
+#include "Window.h"
+#include "Mesh.h"
+#include "Shader_m.h"
+#include "Camera.h"
+#include "Sphere.h"
+#include"Model.h"
+#include "Skybox.h"
+
+const float toRadians = 3.14159265f / 180.0f;
+//float angulocola = 0.0f;
+Window mainWindow;
+std::vector<Mesh*> meshList;
+std::vector<Shader> shaderList;
+
+Camera camera;
+/*Model GoddardCuerpo_M;
+Model Goddardpatafrontal_M;
+Model Goddardpatatrasera_M;
+Model Goddardcola_M;
+Model Goddardmandibulasup_M;
+Model GoddardmandibulaINF_M;*/
+
+Model Buggatti_M;
+Model BuggattilLanta_M;
+Model Buggatticapo_M;
+
+Skybox skybox;
+
+//Sphere cabeza = Sphere(0.5, 20, 20);
+GLfloat deltaTime = 0.0f;
+GLfloat lastTime = 0.0f;
+static double limitFPS = 1.0 / 60.0;
+
+
+// Vertex Shader
+static const char* vShader = "shaders/shader_m.vert";
+
+// Fragment Shader
+static const char* fShader = "shaders/shader_m.frag";
+
+
+
+
+
+void CreateObjects()
+{
+	unsigned int indices[] = {
+		0, 3, 1,
+		1, 3, 2,
+		2, 3, 0,
+		0, 1, 2
+	};
+
+	GLfloat vertices[] = {
+		//	x      y      z			u	  v			nx	  ny    nz
+			-1.0f, -1.0f, -0.6f,	0.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+			0.0f, -1.0f, 1.0f,		0.5f, 0.0f,		0.0f, 0.0f, 0.0f,
+			1.0f, -1.0f, -0.6f,		1.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+			0.0f, 1.0f, 0.0f,		0.5f, 1.0f,		0.0f, 0.0f, 0.0f
+	};
+
+	unsigned int floorIndices[] = {
+		0, 2, 1,
+		1, 2, 3
+	};
+
+	GLfloat floorVertices[] = {
+		-10.0f, 0.0f, -10.0f,	0.0f, 0.0f,		0.0f, -1.0f, 0.0f,
+		10.0f, 0.0f, -10.0f,	10.0f, 0.0f,	0.0f, -1.0f, 0.0f,
+		-10.0f, 0.0f, 10.0f,	0.0f, 10.0f,	0.0f, -1.0f, 0.0f,
+		10.0f, 0.0f, 10.0f,		10.0f, 10.0f,	0.0f, -1.0f, 0.0f
+	};
+
+	
+	Mesh *obj1 = new Mesh();
+	obj1->CreateMesh(vertices, indices, 32, 12);
+	meshList.push_back(obj1);
+
+	Mesh *obj2 = new Mesh();
+	obj2->CreateMesh(vertices, indices, 32, 12);
+	meshList.push_back(obj2);
+
+	Mesh *obj3 = new Mesh();
+	obj3->CreateMesh(floorVertices, floorIndices, 32, 6);
+	meshList.push_back(obj3);
+
+
+}
+
+
+void CreateShaders()
+{
+	Shader *shader1 = new Shader();
+	shader1->CreateFromFiles(vShader, fShader);
+	shaderList.push_back(*shader1);
+}
+
+
+
+int main()
+{
+	mainWindow = Window(1366, 768); // 1280, 1024 or 1024, 768
+	mainWindow.Initialise();
+
+	CreateObjects();
+	CreateShaders();
+
+	camera = Camera(glm::vec3(0.0f, 0.5f, 7.0f), glm::vec3(0.0f, 1.0f, 0.0f), -60.0f, 0.0f, 0.3f, 1.0f);
+
+	/*GoddardCuerpo_M = Model();
+	GoddardCuerpo_M.LoadModel("modelosgoddar/cuerpoorigen.obj");
+	Goddardpatafrontal_M = Model();
+	Goddardpatafrontal_M.LoadModel("modelosgoddar/pataderechaorigen.obj");
+	Goddardpatatrasera_M = Model();
+	Goddardpatatrasera_M.LoadModel("modelosgoddar/patatraseraorigen.obj");
+	Goddardcola_M = Model();
+	Goddardcola_M.LoadModel("modelosgoddar/colaorigen.obj");
+	Goddardmandibulasup_M = Model();
+	Goddardmandibulasup_M.LoadModel("modelosgoddar/mandibulasuperior.obj");
+	GoddardmandibulaINF_M = Model();
+	GoddardmandibulaINF_M.LoadModel("modelosgoddar/mandibulainferior.obj");*/
+
+	Buggatti_M = Model();
+	Buggatti_M.LoadModel("BUGATTI/Carroceria.obj");
+	BuggattilLanta_M = Model();
+	BuggattilLanta_M.LoadModel("BUGATTI/llantaOrigen.obj");
+	Buggatticapo_M = Model();
+	Buggatticapo_M.LoadModel("BUGATTI/CAPOORIGEN.obj");
+
+
+	std::vector<std::string> skyboxFaces;
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_rt.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_lf.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_dn.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_up.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_bk.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_ft.tga");
+
+	skybox = Skybox(skyboxFaces);
+
+	GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0, uniformEyePosition = 0,
+		uniformSpecularIntensity = 0, uniformShininess = 0;
+	GLuint uniformColor = 0;
+	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)mainWindow.getBufferWidth() / mainWindow.getBufferHeight(), 0.1f, 1000.0f);
+
+
+	glm::mat4 model(1.0);
+	glm::mat4 modelaux(1.0);
+
+	glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f);
+
+	////Loop mientras no se cierra la ventana
+	while (!mainWindow.getShouldClose())
+	{
+		GLfloat now = glfwGetTime();
+		deltaTime = now - lastTime;
+		deltaTime += (now - lastTime) / limitFPS;
+		lastTime = now;
+
+		//Recibir eventos del usuario
+		glfwPollEvents();
+		camera.keyControl(mainWindow.getsKeys(), deltaTime);
+		camera.mouseControl(mainWindow.getXChange(), mainWindow.getYChange());
+
+		// Clear the window
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		//Se dibuja el Skybox
+		skybox.DrawSkybox(camera.calculateViewMatrix(), projection);
+
+		shaderList[0].UseShader();
+		uniformModel = shaderList[0].GetModelLocation();
+		uniformProjection = shaderList[0].GetProjectionLocation();
+		uniformView = shaderList[0].GetViewLocation();
+		uniformColor = shaderList[0].getColorLocation();
+
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(camera.calculateViewMatrix()));
+		// INICIA DIBUJO DEL PISO
+		color = glm::vec3(0.5f, 0.5f, 0.5f); //piso de color gris
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(0.0f, -2.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(30.0f, 1.0f, 30.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		meshList[2]->RenderMesh();
+
+
+		model = glm::mat4(1.0);
+
+		model = glm::translate(model, glm::vec3(0.0f, -1.0f, 0.0f));
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.1f) * mainWindow.getarticulacion1());
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.1f) * mainWindow.getarticulacion2());
+
+
+
+		modelaux = model;
+
+		color = glm::vec3(1.0f, 0.0f, 0.0f); 
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+
+		Buggatti_M.RenderModel();
+
+		model = modelaux;
+
+		model = glm::translate(model, glm::vec3(2.5f, 0.0f, 2.8f));
+
+		modelaux = model;
+
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion1()), glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion2()), glm::vec3(1.0f, 0.0f, 0.0f));
+				color = glm::vec3(0.0f, 1.0f, 0.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		BuggattilLanta_M.RenderModel();
+
+		model = modelaux;
+
+		model = glm::translate(model, glm::vec3(-5.0f, 0.0f, 0.0f));
+
+		modelaux = model;
+
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion1()), glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion2()), glm::vec3(-1.0f, 0.0f, 0.0f));
+		color = glm::vec3(.0f, 1.0f, 0.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		BuggattilLanta_M.RenderModel();
+
+		model = modelaux;
+
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, -7.8f));
+
+		modelaux = model;
+
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion1()), glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion2()), glm::vec3(-1.0f, 0.0f, 0.0f));
+		color = glm::vec3(.0f, 1.0f, 0.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		BuggattilLanta_M.RenderModel();
+
+		model = modelaux;
+
+		model = glm::translate(model, glm::vec3(5.0f, 0.0f, 0.0f));
+
+		modelaux = model;
+
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion1()), glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion2()), glm::vec3(-1.0f, 0.0f, 0.0f));
+		color = glm::vec3(.0f, 1.0f, 0.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		BuggattilLanta_M.RenderModel();
+
+		model = modelaux;
+
+		model = glm::translate(model, glm::vec3(-2.3f, 1.6f, 7.1f));
+
+		modelaux = model;
+
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion3()), glm::vec3(1.0f, 0.0f, 0.0f));
+		color = glm::vec3(0.5f, 0.0f, 0.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Buggatticapo_M.RenderModel();
+
+		model = modelaux;
+
+
+		/*//------------*INICIA DIBUJO DE NUESTROS DEMÁS OBJETOS-------------------*
+		//Goddard
+
+
+		model = glm::mat4(1.0);
+
+		model = glm::translate(model, glm::vec3(0.0f, 0.4f, 0.0f));
+
+		modelaux = model;
+
+		color = glm::vec3(0.2f, 0.8f, 0.2f); //modelo de goddard de color negro
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+
+		GoddardCuerpo_M.RenderModel();//modificar por el modelo sin las 4 patas y sin cola
+
+		model = modelaux;
+
+		model = glm::translate(model, glm::vec3(1.3f, -0.3f, 0.7f));
+
+		modelaux = model;
+
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion1()), glm::vec3(0.0f, 0.0f, 1.0f));
+		color = glm::vec3(1.0f, 0.0f, 0.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Goddardpatafrontal_M.RenderModel();//modificar por el modelo sin las 4 patas y sin cola
+
+		model = modelaux;
+
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, -1.4f));
+
+		modelaux = model;
+
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion2()), glm::vec3(0.0f, 0.0f, 1.0f));
+		color = glm::vec3(0.0f, 0.0f, 1.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Goddardpatafrontal_M.RenderModel();//modificar por el modelo sin las 4 patas y sin cola
+
+		model = modelaux;
+
+		model = glm::translate(model, glm::vec3(-1.6f, -0.7f, 0.0f));
+
+		modelaux = model;
+
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion3()), glm::vec3(0.0f, 0.0f, 1.0f));
+		color = glm::vec3(1.0f, 0.75f, 0.8f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Goddardpatatrasera_M.RenderModel();//modificar por el modelo sin las 4 patas y sin cola
+
+		model = modelaux;
+
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 1.4f));
+
+		modelaux = model;
+
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion4()), glm::vec3(0.0f, 0.0f, 1.0f));
+		color = glm::vec3(0.5f, 0.0f, 0.5f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Goddardpatatrasera_M.RenderModel();//modificar por el modelo sin las 4 patas y sin cola
+
+		model = modelaux;
+
+		model = glm::translate(model, glm::vec3(-1.0f, 0.9f, -0.7f));
+
+		modelaux = model;
+
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion5()), glm::vec3(0.0f, 1.0f, 0.0f));
+		color = glm::vec3(0.25f, 0.88f, 0.82f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Goddardcola_M.RenderModel();//modificar por el modelo sin las 4 patas y sin cola
+
+		model = modelaux;
+
+		model = glm::translate(model, glm::vec3(4.0f, 1.2f, 0.0f));
+
+		modelaux = model;
+
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion6()), glm::vec3(0.0f, 0.0f, 1.0f));
+		color = glm::vec3(0.0f, 0.28f, 0.67f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Goddardmandibulasup_M.RenderModel();//modificar por el modelo sin las 4 patas y sin cola
+
+		model = modelaux;
+	
+		modelaux = model;
+
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion7()), glm::vec3(0.0f, 0.0f, 1.0f));
+		color = glm::vec3(0.31f, 0.78f, 0.47f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		GoddardmandibulaINF_M.RenderModel();//modificar por el modelo sin las 4 patas y sin cola
+
+		model = modelaux;
+
+		*/
+
+
+
+
+		glUseProgram(0);
+
+		mainWindow.swapBuffers();
+	}
+
+	return 0;
+}

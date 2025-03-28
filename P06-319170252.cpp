3c5ee18b2d511b579604ef75254fc6600e00bc45
Moreno-S-Mariana <90338791+Moreno-S-Mariana@users.noreply.github.com>
2025-03-27 22:42:12 -0600

    Add files via upload

---
+++ P06-319170252.cpp
@@ -0,0 +1,542 @@
+/*
+Práctica 6: Texturizado
+*/
+//para cargar imagen
+#define STB_IMAGE_IMPLEMENTATION
+
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#include <vector>
+#include <math.h>
+
+#include <glew.h>
+#include <glfw3.h>
+
+#include <glm.hpp>
+#include <gtc\matrix_transform.hpp>
+#include <gtc\type_ptr.hpp>
+//para probar el importer
+//#include<assimp/Importer.hpp>
+
+#include "Window.h"
+#include "Mesh.h"
+#include "Shader_m.h"
+#include "Camera.h"
+#include "Texture.h"
+#include "Sphere.h"
+#include"Model.h"
+#include "Skybox.h"
+
+const float toRadians = 3.14159265f / 180.0f;
+
+Window mainWindow;
+std::vector<Mesh*> meshList;
+std::vector<Shader> shaderList;
+
+Camera camera;
+
+Texture brickTexture;
+Texture dirtTexture;
+Texture plainTexture;
+Texture pisoTexture;
+Texture dadoTexture;
+Texture dadoTexture_Animales;
+Texture logofiTexture;
+Texture Dado_Ocho;
+
+Model Kitt_M;
+Model Llanta_M;
+Model Dado_M;
+Model cubo_M; 
+Model BuggattilLanta_M;
+Model Buggatticapo_M;
+Model Buggatti_M;
+
+Skybox skybox;
+
+//Sphere cabeza = Sphere(0.5, 20, 20);
+GLfloat deltaTime = 0.0f;
+GLfloat lastTime = 0.0f;
+static double limitFPS = 1.0 / 60.0;
+
+
+// Vertex Shader
+static const char* vShader = "shaders/shader_texture.vert";
+
+// Fragment Shader
+static const char* fShader = "shaders/shader_texture.frag";
+
+
+
+
+//cálculo del promedio de las normales para sombreado de Phong
+void calcAverageNormals(unsigned int* indices, unsigned int indiceCount, GLfloat* vertices, unsigned int verticeCount,
+	unsigned int vLength, unsigned int normalOffset)
+{
+	for (size_t i = 0; i < indiceCount; i += 3)
+	{
+		unsigned int in0 = indices[i] * vLength;
+		unsigned int in1 = indices[i + 1] * vLength;
+		unsigned int in2 = indices[i + 2] * vLength;
+		glm::vec3 v1(vertices[in1] - vertices[in0], vertices[in1 + 1] - vertices[in0 + 1], vertices[in1 + 2] - vertices[in0 + 2]);
+		glm::vec3 v2(vertices[in2] - vertices[in0], vertices[in2 + 1] - vertices[in0 + 1], vertices[in2 + 2] - vertices[in0 + 2]);
+		glm::vec3 normal = glm::cross(v1, v2);
+		normal = glm::normalize(normal);
+
+		in0 += normalOffset; in1 += normalOffset; in2 += normalOffset;
+		vertices[in0] += normal.x; vertices[in0 + 1] += normal.y; vertices[in0 + 2] += normal.z;
+		vertices[in1] += normal.x; vertices[in1 + 1] += normal.y; vertices[in1 + 2] += normal.z;
+		vertices[in2] += normal.x; vertices[in2 + 1] += normal.y; vertices[in2 + 2] += normal.z;
+	}
+
+	for (size_t i = 0; i < verticeCount / vLength; i++)
+	{
+		unsigned int nOffset = i * vLength + normalOffset;
+		glm::vec3 vec(vertices[nOffset], vertices[nOffset + 1], vertices[nOffset + 2]);
+		vec = glm::normalize(vec);
+		vertices[nOffset] = vec.x; vertices[nOffset + 1] = vec.y; vertices[nOffset + 2] = vec.z;
+	}
+}
+
+
+
+void CreateObjects()
+{
+	unsigned int indices[] = {
+		0, 3, 1,
+		1, 3, 2,
+		2, 3, 0,
+		0, 1, 2
+	};
+
+	GLfloat vertices[] = {
+		//	x      y      z			u	  v			nx	  ny    nz
+			-1.0f, -1.0f, -0.6f,	0.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+			0.0f, -1.0f, 1.0f,		0.5f, 0.0f,		0.0f, 0.0f, 0.0f,
+			1.0f, -1.0f, -0.6f,		1.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+			0.0f, 1.0f, 0.0f,		0.5f, 1.0f,		0.0f, 0.0f, 0.0f
+	};
+
+	unsigned int floorIndices[] = {
+		0, 2, 1,
+		1, 2, 3
+	};
+
+	GLfloat floorVertices[] = {
+		-10.0f, 0.0f, -10.0f,	0.0f, 0.0f,		0.0f, -1.0f, 0.0f,
+		10.0f, 0.0f, -10.0f,	10.0f, 0.0f,	0.0f, -1.0f, 0.0f,
+		-10.0f, 0.0f, 10.0f,	0.0f, 10.0f,	0.0f, -1.0f, 0.0f,
+		10.0f, 0.0f, 10.0f,		10.0f, 10.0f,	0.0f, -1.0f, 0.0f
+	};
+
+	unsigned int vegetacionIndices[] = {
+		0, 1, 2,
+		0, 2, 3,
+		4,5,6,
+		4,6,7
+	};
+
+	GLfloat vegetacionVertices[] = {
+		-0.5f, -0.5f, 0.0f,		0.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, 0.0f,		1.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+		0.5f, 0.5f, 0.0f,		1.0f, 1.0f,		0.0f, 0.0f, 0.0f,
+		-0.5f, 0.5f, 0.0f,		0.0f, 1.0f,		0.0f, 0.0f, 0.0f,
+
+		0.0f, -0.5f, -0.5f,		0.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+		0.0f, -0.5f, 0.5f,		1.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+		0.0f, 0.5f, 0.5f,		1.0f, 1.0f,		0.0f, 0.0f, 0.0f,
+		0.0f, 0.5f, -0.5f,		0.0f, 1.0f,		0.0f, 0.0f, 0.0f,
+	};
+	calcAverageNormals(indices, 12, vertices, 32, 8, 5);
+
+
+	
+	Mesh *obj1 = new Mesh();
+	obj1->CreateMesh(vertices, indices, 32, 12);
+	meshList.push_back(obj1);
+
+	Mesh *obj2 = new Mesh();
+	obj2->CreateMesh(vertices, indices, 32, 12);
+	meshList.push_back(obj2);
+
+	Mesh *obj3 = new Mesh();
+	obj3->CreateMesh(floorVertices, floorIndices, 32, 6);
+	meshList.push_back(obj3);
+
+	Mesh* obj4 = new Mesh();
+	obj4->CreateMesh(vegetacionVertices, vegetacionIndices, 64, 12);
+	meshList.push_back(obj4);
+
+}
+
+void CreateShaders()
+{
+	Shader *shader1 = new Shader();
+	shader1->CreateFromFiles(vShader, fShader);
+	shaderList.push_back(*shader1);
+}
+
+void CrearDado()
+{
+	unsigned int cubo_indices[] = {
+		// front
+		0, 1, 2,
+		2, 3, 0,
+		
+		// back
+		8, 9, 10,
+		10, 11, 8,
+
+		// left
+		12, 13, 14,
+		14, 15, 12,
+		// bottom
+		16, 17, 18,
+		18, 19, 16,
+		// top
+		20, 21, 22,
+		22, 23, 20,
+
+		// right
+		4, 5, 6,
+		6, 7, 4,
+
+	};
+	//Ejercicio 1: reemplazar con sus dados de 6 caras texturizados, agregar normales
+	// average normals
+	GLfloat cubo_vertices[] = {
+
+		// front (cara delantera: z = 0.5, normal = (0,0,1))
+		//x      y       z       S       T           NX      NY      NZ
+		- 0.5f, -0.5f,  0.5f,   0.260f,  0.375f,   0.0f,   0.0f,   1.0f,   //0
+		0.5f, -0.5f,  0.5f,    0.500f,  0.375f,   0.0f,   0.0f,   1.0f,   //1
+		0.5f,  0.5f,  0.5f,    0.500f,  0.620f,   0.0f,   0.0f,   1.0f,   //2
+		-0.5f,  0.5f,  0.5f,   0.260f,  0.620f,   0.0f,   0.0f,   1.0f,   //3
+
+		// right (cara derecha: x = 0.5, normal = (1,0,0))
+		//x      y       z       S       T           NX      NY      NZ
+		0.5f, -0.5f,  0.5f,    0.520f,  0.375f,   1.0f,   0.0f,   0.0f,
+		0.5f, -0.5f, -0.5f,    0.750f,  0.375f,   1.0f,   0.0f,   0.0f,
+		0.5f,  0.5f, -0.5f,    0.750f,  0.620f,   1.0f,   0.0f,   0.0f,
+		0.5f,  0.5f,  0.5f,    0.520f,  0.620f,   1.0f,   0.0f,   0.0f,
+
+		// back (cara trasera: z = -0.5, normal = (0,0,-1))
+		//x      y       z       S       T           NX      NY      NZ
+		-0.5f, -0.5f, -0.5f,   0.750f,  0.375f,   0.0f,   0.0f,   -1.0f,
+		0.5f, -0.5f, -0.5f,    1.0f,    0.375f,   0.0f,   0.0f,   -1.0f,
+		0.5f,  0.5f, -0.5f,    1.0f,    0.620f,   0.0f,   0.0f,   -1.0f,
+		-0.5f,  0.5f, -0.5f,   0.750f,  0.620f,   0.0f,   0.0f,   -1.0f,
+
+		// left (cara izquierda: x = -0.5, normal = (-1,0,0))
+		//x      y       z       S       T           NX      NY      NZ
+		-0.5f, -0.5f, -0.5f,   0.01f,   0.375f,   -1.0f,  0.0f,   0.0f,
+		-0.5f, -0.5f,  0.5f,   0.250f,  0.375f,   -1.0f,  0.0f,   0.0f,
+		-0.5f,  0.5f,  0.5f,   0.250f,  0.620f,   -1.0f,  0.0f,   0.0f,
+		-0.5f,  0.5f, -0.5f,   0.01f,   0.620f,   -1.0f,  0.0f,   0.0f,
+
+		// bottom (cara inferior: y = -0.5, normal = (0,-1,0))
+		// x      y       z       S       T           NX      NY      NZ
+		-0.5f, -0.5f,  0.5f,   0.260f,  0.370f,   0.0f,   -1.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,   0.490f,  0.370f,   0.0f,   -1.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,   0.490f,  0.125f,   0.0f,   -1.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f,   0.260f,  0.125f,   0.0f,   -1.0f,  0.0f,
+
+
+		// UP (cara superior: y = 0.5, normal = (0,1,0))
+		//x      y       z       S       T           NX      NY      NZ
+		-0.5f,  0.5f,  0.5f,   0.260f,  0.625f,   0.0f,   1.0f,   0.0f,
+		0.5f,   0.5f,  0.5f,   0.500f,  0.625f,   0.0f,   1.0f,   0.0f,
+		0.5f,   0.5f, -0.5f,   0.500f,  0.870f,   0.0f,   1.0f,   0.0f,
+		-0.5f,  0.5f, -0.5f,   0.260f,  0.870f,   0.0f,   1.0f,   0.0f,
+	};
+
+	// Llamada a la función para calcular el promedio de las normales (para sombreado Phong)
+	// Parámetros:
+	// - cubo_indices: array de índices.
+	// - 36: cantidad de índices.
+	// - cubo_vertices: array de vértices.
+	// - 192: cantidad de elementos en el array de vértices.
+	// - 8: cantidad de floats por vértice.
+	// - 5: offset donde comienzan las normales (después de x, y, z, S, T)
+	calcAverageNormals(cubo_indices, 36, cubo_vertices, 192, 8, 5);
+
+	Mesh* dado = new Mesh();
+	dado->CreateMesh(cubo_vertices, cubo_indices, 192, 36);
+	meshList.push_back(dado);
+
+}
+
+void CrearDado_OCHO()
+{
+	// Definición de los índices (8 caras * 3 vértices = 24 índices)
+	unsigned int octahedron_indices[] = {
+		 0,  1,  2,   // Cara 1 (T1)
+		 3,  4,  5,   // Cara 2 (T2)
+		 6,  7,  8,   // Cara 3 (T3)
+		 9, 10, 11,   // Cara 4 (T4)
+		12, 13, 14,   // Cara 5 (T5)
+		15, 16, 17,   // Cara 6 (T6)
+		18, 19, 20,   // Cara 7 (T7)
+		21, 22, 23    // Cara 8 (T8)
+	};
+
+
+	GLfloat octahedron_vertices[] = {
+
+	//1
+	 0.0f,   0.0f,  0.5f,    0.05f, 0.79f,    0.5774f,  0.5774f,  0.5774f,
+	 0.5f,   0.0f,  0.0f,    0.23f, 0.79f,    0.5774f,  0.5774f,  0.5774f,
+	 0.0f,   0.5f,  0.0f,    0.125f, 1.0f,    0.5774f,  0.5774f,  0.5774f,
+	//4
+	 0.0f,  0.0f,  0.5f,    0.96f,  0.79f,   -0.5774f,  0.5774f,  0.5774f,
+	 0.0f,  0.5f,  0.0f,    0.86f,  1.0f,	 -0.5774f,  0.5774f,  0.5774f,
+	-0.5f,  0.0f,  0.0f,    0.75f,  0.79f,   -0.5774f,  0.5774f,  0.5774f,
+	//8
+	 0.0f,  0.0f,  0.5f,    0.96f, 0.77f,    -0.5774f, -0.5774f,  0.5774f,
+	 -0.5f, 0.0f,  0.0f,    0.75f, 0.77f,    -0.5774f, -0.5774f,  0.5774f,
+	 0.0f, -0.5f,  0.0f,    0.85f, 0.60f,    -0.5774f, -0.5774f,  0.5774f,
+	 //5
+     0.0f,  0.0f,  0.5f,    0.05f,  0.77f,     0.5774f, -0.5774f,  0.5774f,
+	 0.0f, -0.5f,  0.0f,    0.125f, 0.60f,     0.5774f, -0.5774f,  0.5774f,
+	 0.5f,  0.0f,  0.0f,    0.23f,  0.77f,     0.5774f, -0.5774f,  0.5774f,
+	 //2
+	 0.0f,  0.0f, -0.5f,    0.47f, 0.79f,      0.5774f,  0.5774f, -0.5774f,
+	 0.0f,  0.5f,  0.0f,    0.37f,  0.99f,     0.5774f,  0.5774f, -0.5774f,
+	 0.5f,  0.0f,  0.0f,    0.28f,  0.79f,     0.5774f,  0.5774f, -0.5774f,
+	 //3
+	 0.0f,  0.0f, -0.5f,    0.51f, 0.79f,     -0.5774f,  0.5774f, -0.5774f,
+	-0.5f,  0.0f,  0.0f,    0.69f,  0.79f,    -0.5774f,  0.5774f, -0.5774f,
+	 0.0f,  0.5f,  0.0f,    0.615f,   1.0f,   -0.5774f,  0.5774f, -0.5774f,
+	 //7
+	 0.0f,  0.0f, -0.5f,   0.51f, 0.78f,      -0.5774f, -0.5774f, -0.5774f,
+	 0.0f, -0.5f,  0.0f,   0.615f,  0.60f,    -0.5774f, -0.5774f, -0.5774f,
+	-0.5f,  0.0f,  0.0f,   0.69f,  0.78f,     -0.5774f, -0.5774f, -0.5774f,
+	 //6
+	 0.0f,  0.0f, -0.5f,   0.45f, 0.78f,      0.5774f, -0.5774f, -0.5774f,
+	 0.5f,  0.0f,  0.0f,   0.28f,   0.78f,      0.5774f, -0.5774f, -0.5774f,
+	 0.0f, -0.5f,  0.0f,   0.37f,  0.60f,      0.5774f, -0.5774f, -0.5774f
+	};
+
+	calcAverageNormals(octahedron_indices, 24, octahedron_vertices, 192, 8, 5);
+	Mesh* dado_OCHO = new Mesh();
+	dado_OCHO->CreateMesh(octahedron_vertices, octahedron_indices, 192, 24);
+	meshList.push_back(dado_OCHO);
+}
+
+
+
+
+
+int main()
+{
+	mainWindow = Window(1366, 768); // 1280, 1024 or 1024, 768
+	mainWindow.Initialise();
+
+	CreateObjects();
+	CrearDado();
+	CrearDado_OCHO();
+	CreateShaders();
+
+	camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -60.0f, 0.0f, 0.3f, 0.5f);
+
+	brickTexture = Texture("Textures/brick.png");
+	brickTexture.LoadTextureA();
+	dirtTexture = Texture("Textures/dirt.png");
+	dirtTexture.LoadTextureA();
+	plainTexture = Texture("Textures/plain.png");
+	plainTexture.LoadTextureA();
+	pisoTexture = Texture("Textures/piso.tga");
+	pisoTexture.LoadTextureA();
+	dadoTexture = Texture("Textures/dado-de-numeros.png");
+	dadoTexture.LoadTextureA();
+	dadoTexture_Animales = Texture("Textures/dado_animalesblender.tga");
+	dadoTexture_Animales.LoadTextureA();
+	logofiTexture = Texture("Textures/escudo_fi_color.tga");
+	logofiTexture.LoadTextureA();
+
+	//////////////////////////////TEXTURA DADO OCHO CARAS 
+
+	Dado_Ocho = Texture("Textures/cubocompleto.tga");
+	Dado_Ocho.LoadTextureA();
+	
+	
+	Kitt_M = Model();
+	Kitt_M.LoadModel("Models/kitt_optimizado.obj");
+	Llanta_M = Model();
+	Llanta_M.LoadModel("Models/llanta_optimizada.obj");
+	Dado_M = Model();
+	Dado_M.LoadModel("Models/dado_textures.obj");
+	cubo_M = Model();
+	cubo_M.LoadModel("Models/cubo_texture.obj");
+	BuggattilLanta_M = Model();
+	BuggattilLanta_M.LoadModel("Models/llantaOrigen.obj");
+	Buggatticapo_M = Model();
+	Buggatticapo_M.LoadModel("Models/cofretex.obj");
+	Buggatti_M = Model();
+	Buggatti_M.LoadModel("Models/panic1.obj");
+	
+	std::vector<std::string> skyboxFaces;
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_rt.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_lf.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_dn.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_up.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_bk.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_ft.tga");
+
+	skybox = Skybox(skyboxFaces);
+
+	GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0, uniformEyePosition = 0,
+		uniformSpecularIntensity = 0, uniformShininess = 0;
+	GLuint uniformColor = 0;
+	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)mainWindow.getBufferWidth() / mainWindow.getBufferHeight(), 0.1f, 1000.0f);
+	
+	glm::mat4 model(1.0);
+	glm::mat4 modelaux(1.0);
+	glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f);
+	////Loop mientras no se cierra la ventana
+	while (!mainWindow.getShouldClose())
+	{
+		GLfloat now = glfwGetTime();
+		deltaTime = now - lastTime;
+		deltaTime += (now - lastTime) / limitFPS;
+		lastTime = now;
+
+		//Recibir eventos del usuario
+		glfwPollEvents();
+		camera.keyControl(mainWindow.getsKeys(), deltaTime);
+		camera.mouseControl(mainWindow.getXChange(), mainWindow.getYChange());
+
+		// Clear the window
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		skybox.DrawSkybox(camera.calculateViewMatrix(), projection);
+		shaderList[0].UseShader();
+		uniformModel = shaderList[0].GetModelLocation();
+		uniformProjection = shaderList[0].GetProjectionLocation();
+		uniformView = shaderList[0].GetViewLocation();
+		uniformColor = shaderList[0].getColorLocation();
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(camera.calculateViewMatrix()));
+		glUniform3f(uniformEyePosition, camera.getCameraPosition().x, camera.getCameraPosition().y, camera.getCameraPosition().z);
+
+		color = glm::vec3(1.0f, 1.0f, 1.0f);//color blanco, multiplica a la información de color de la textura
+
+		
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(0.0f, -2.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(30.0f, 1.0f, 30.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+
+		pisoTexture.UseTexture();
+		meshList[2]->RenderMesh();
+
+
+		/*//Dado de Opengl
+		//Ejercicio 1: Texturizar su cubo con la imagen dado_animales ya optimizada por ustedes
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(-1.5f, 4.5f, -2.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		dadoTexture_Animales.UseTexture();
+		meshList[4]->RenderMesh();
+		
+		//Ejercicio 2:Importar el cubo texturizado en el programa de modelado con 
+		//la imagen dado_animales ya optimizada por ustedes
+		
+		//Dado importado
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(-3.0f, 3.0f, -2.0f));
+		model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));	
+		//model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		cubo_M.RenderModel();*/
+
+		
+		
+		/*Reporte de práctica :
+		Ejercicio 1: Crear un dado de 8 caras y texturizarlo por medio de código
+		Ejercicio 2: Importar el modelo de su coche con sus 4 llantas acomodadas
+		y tener texturizadas las 4 llantas (diferenciar caucho y rin)  y 
+		texturizar el logo de la Facultad de ingeniería en el cofre de su propio modelo de coche
+	
+		*/
+
+		//********************************* EJERCICIO 1 *****************************************
+
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(-1.5f, 4.5f, -2.0f));
+		model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Dado_Ocho.UseTexture();
+		meshList[5]->RenderMesh();
+
+		//carroceria del automovil
+		
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(0.0f, -1.0f, 0.0f));
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.1f) * mainWindow.getarticulacion1()); //traslacion del cuerpo adelante
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.1f) * mainWindow.getarticulacion2()); //traslacion del cuerpo hacia atras
+		modelaux = model;
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Buggatti_M.RenderModel();
+
+		model = modelaux;
+		
+		model = glm::translate(model, glm::vec3(2.5f, 0.7f, 2.825f));
+		model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion1()), glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion2()), glm::vec3(1.0f, 0.0f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		BuggattilLanta_M.RenderModel();
+		
+		model = modelaux;
+
+		model = glm::translate(model, glm::vec3(-2.5f, 0.7f, 2.825f));
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion1()), glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion2()), glm::vec3(1.0f, 0.0f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		BuggattilLanta_M.RenderModel();
+
+		model = modelaux;
+
+		model = glm::translate(model, glm::vec3(-2.5f, 0.7f, -4.975f));
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion1()), glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion2()), glm::vec3(1.0f, 0.0f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		BuggattilLanta_M.RenderModel();
+
+		model = modelaux;
+
+		model = glm::translate(model, glm::vec3(2.5f, 0.7f, -4.975f));
+		model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion1()), glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion2()), glm::vec3(1.0f, 0.0f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		BuggattilLanta_M.RenderModel();
+
+		model = modelaux;
+
+		model = glm::translate(model, glm::vec3(0.3f, 1.6f, 2.0f));
+		model = glm::rotate(model, glm::radians(mainWindow.getarticulacion3()), glm::vec3(1.0f, 0.0f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Buggatticapo_M.RenderModel();
+
+
+
+		glUseProgram(0);
+
+		mainWindow.swapBuffers();
+	}
+
+	return 0;
+}
+/*
+//blending: transparencia o traslucidez
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		logofiTexture.UseTexture(); //textura con transparencia o traslucidez
+		FIGURA A RENDERIZAR de OpenGL, si es modelo importado no se declara UseTexture
+		glDisable(GL_BLEND);
+*/
